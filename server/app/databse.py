93b683a70edfc4fd0ab832c94bfc97bf4ac6be75
Mitvi Shah <104480110+shahMitvi@users.noreply.github.com>
2026-03-12 18:34:07 +0530

    Change database name to use environment variable
    Updated database connection settings to use environment variable for database name.

--- server/app/databse.py
+++ server/app/databse.py
@@ -6,11 +6,11 @@ import certifi
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGODB_URL")
-# DB_NAME = os.getenv("DB_NAME","shahmitvi42_db_user")
+# DB_NAME = os.getenv("DB_NAME","your_db_name")
 
 ca = certifi.where()
 client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=ca)
-db = client["shahmitvi42_db_user"]
+db = client["your_db_name"]
 
 def get_db():
     return db
